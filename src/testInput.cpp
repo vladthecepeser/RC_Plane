6d91e8dd2a41c705dae050fdc9371f5ee3b35967
vladthecepeser <gligovla@gmail.com>
2026-08-01 22:29:45 -0500

    Finished working code for RC plane

---
+++ src/testInput.cpp
@@ -0,0 +1,186 @@
+// #include <Arduino.h>
+// #include <SPI.h>
+// #include <RF24.h>
+// #include <ESP32Servo.h>
+
+// // Servo objects
+// Servo leftWing;
+// Servo rightWing;
+
+// // NRF24L01+ pins
+// #define MY_MISO 22
+// #define MY_MOSI 21
+// #define MY_SCK 19
+// #define MY_CSN 18
+// #define MY_CE 4
+
+// //Servo Pins
+// const int rightWingPin = 26;
+// const int leftWingPin = 27;
+
+// void resetSequence();
+// void resetChip();
+
+// struct Packet {
+//     uint16_t rightHorizontal;
+//     uint16_t rightVertical;
+//     uint16_t leftHorizontal;
+//     uint16_t leftVertical;
+//     uint8_t rightR;
+//     uint8_t rightL;
+//     uint8_t leftR;
+//     uint8_t leftL;
+// };
+
+// RF24 radio(MY_CE, MY_CSN, 1000000);
+// SPIClass *vspi = new SPIClass(VSPI);
+// const uint64_t pipeAddress = 0xF0F0F0F0D2LL;
+// const uint8_t payloadSize = sizeof(Packet);
+
+// void setup() {
+
+//     // Allow allocation of all timers
+// 	ESP32PWM::allocateTimer(0);
+// 	ESP32PWM::allocateTimer(1);
+// 	ESP32PWM::allocateTimer(2);
+// 	ESP32PWM::allocateTimer(3);
+
+//     Serial.begin(115200);
+
+//     vspi->begin(MY_SCK, MY_MISO, MY_MOSI, MY_CSN);
+//     pinMode(MY_CE, OUTPUT);
+//     pinMode(MY_CSN, OUTPUT);
+
+//     if (!radio.begin(vspi, MY_CE, MY_CSN)) {
+//         Serial.println("RADIO INIT FAILED");
+//         while (1) {
+//             delay(10);
+//         }
+//     }
+
+//     resetSequence();
+
+// 	leftWing.setPeriodHertz(50);      // Standard 50hz servo
+// 	rightWing.setPeriodHertz(50);      // Standard 50hz servo   
+
+//     Serial.println("Receiver ready");
+// }
+
+// unsigned long sendTime = millis();
+
+// void loop() {
+//     int currentTime = millis();
+
+//     if (!leftWing.attached() || !rightWing.attached()) {
+//         leftWing.attach(leftWingPin, 1000, 2000);
+//         rightWing.attach(rightWingPin, 1000, 2000);
+// 	}
+
+//     uint8_t pipe = 255;
+//     if (radio.available(&pipe)) {
+//         Packet packet;
+//         uint8_t bytes = radio.getPayloadSize();
+
+//         if (bytes == sizeof(packet)) {
+//             radio.read(&packet, bytes);
+
+//             (0< packet.rightHorizontal && packet.rightHorizontal < 190) ? packet.rightHorizontal = 0 : packet.rightHorizontal = packet.rightHorizontal;            //Most Left
+//             (3845 < packet.rightHorizontal && packet.rightHorizontal < 4095) ? packet.rightHorizontal = 3900 : packet.rightHorizontal = packet.rightHorizontal;    //Most Right
+
+//             (3845 < packet.rightVertical && packet.rightVertical < 4095) ? packet.rightVertical = 3900 : packet.rightVertical = packet.rightVertical;          //Most Up
+//             (0< packet.rightVertical && packet.rightVertical < 100) ? packet.rightVertical = 0 : packet.rightVertical = packet.rightVertical;                  //MostDown
+
+//             (4000 < packet.leftHorizontal && packet.leftHorizontal < 4050) ? packet.leftHorizontal = 4095 : packet.leftHorizontal = packet.leftHorizontal;       //Most left
+//             (0< packet.leftHorizontal && packet.leftHorizontal < 150) ? packet.leftHorizontal = 0 : packet.leftHorizontal = packet.leftHorizontal;               //Most Right
+            
+//             (0< packet.leftVertical && packet.leftVertical < 190) ? packet.leftVertical = 0 : packet.leftVertical = packet.leftVertical;                     //Most Up
+//             (3850 < packet.leftVertical &&  packet.leftVertical < 4095) ? packet.leftVertical = 3900 : packet.leftVertical = packet.leftVertical;             //Most Down
+
+//             packet.rightHorizontal = map(packet.rightHorizontal, 0, 3900, 1000, 2000);
+//             packet.rightVertical = map(packet.rightVertical, 0, 3900, 1000, 2000);
+
+//             packet.leftHorizontal = map(packet.leftHorizontal, 0, 4095, 100, 0);
+//             packet.leftVertical = map(packet.leftVertical, 0, 3900, 2000, 1000);
+
+//             leftWing.write(packet.rightHorizontal);
+//             rightWing.write(packet.rightHorizontal);
+
+//             if (currentTime - sendTime >= 100) {
+//                 sendTime = currentTime;
+
+//                 Serial.print("Received ");
+//                 Serial.print(bytes);
+//                 Serial.print(" bytes on pipe ");
+//                 Serial.print(pipe);
+//                 Serial.print(" | RH=");
+//                 Serial.print(packet.rightHorizontal);
+//                 Serial.print(" RV=");
+//                 Serial.print(packet.rightVertical);
+//                 Serial.print(" LH=");
+//                 Serial.print(packet.leftHorizontal);
+//                 Serial.print(" LV=");
+//                 Serial.print(packet.leftVertical);
+//                 Serial.print(" RR=");
+//                 Serial.print(packet.rightR);
+//                 Serial.print(" RL=");
+//                 Serial.print(packet.rightL);
+//                 Serial.print(" LR=");
+//                 Serial.print(packet.leftR);
+//                 Serial.print(" LL=");
+//                 Serial.println(packet.leftL);
+//             }
+            
+//         } else {
+//             Serial.print("Invalid payload size: ");
+//             Serial.println(bytes);
+//             radio.flush_rx();
+//             delay(10);
+//         }
+//     }
+
+//     if (!radio.isChipConnected()) {
+//         resetChip();
+
+//     }
+// }
+
+// void resetSequence(){
+//     radio.setChannel(76);
+//     radio.setDataRate(RF24_1MBPS);
+//     radio.setCRCLength(RF24_CRC_16);
+//     radio.setRetries(5, 15);
+//     radio.setAutoAck(true);
+//     radio.disableDynamicPayloads();
+//     radio.flush_tx();
+//     radio.flush_rx();
+//     radio.openReadingPipe(0, pipeAddress);
+//     radio.setPALevel(RF24_PA_MIN);
+//     radio.setPayloadSize(payloadSize);
+//     radio.startListening();
+// }
+
+// void resetChip(){
+
+//     Serial.println("Resetting radio chip...");
+    
+//     radio.powerDown();
+//     delay(500);     //Important!!!
+
+//     if (!radio.begin(vspi, MY_CE, MY_CSN)) {
+//         Serial.println("Radio chip re-initialization failed");
+//         return;
+//     }
+
+//     //Go thru the flow
+//     resetSequence();
+
+//     if (!radio.isChipConnected()) {
+//         Serial.println("Failed re-initialization");
+//         resetChip();
+//     } 
+//     else {
+//         Serial.println("Successful re-initialization");
+//     }
+
+//     delay(10);
+// }
